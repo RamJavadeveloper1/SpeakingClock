bfb24c9fafdd41e8014d6d345538fd21b0a0b160
Ramkishorgautam1 <ramkishorgautam1@gmail.com>
2023-01-04 12:46:24 +0530

    Added getTime By time fomate input new api

--- src/main/java/com/speaking/clock/controller/ClockController.java
+++ src/main/java/com/speaking/clock/controller/ClockController.java
@@ -25,17 +25,39 @@ public class ClockController {
 	Logger logger = LoggerFactory.getLogger(ClockController.class);
 
 	@GetMapping("/currentTime")
-	private String getTimetOnTextFormate() throws InvalidTimeInputException  {
+	private String getTimetOnTextFormate() throws InvalidTimeInputException {
 		logger.info("ClockController :: called getTimeOnTextFormate function");
 		Date date = new Date();
 		return service.getCurrentTimeOnTextFormate(date.getHours(), date.getMinutes());
 	}
-	
-	
+
+	// Rest Api
+	// http://localhost:8083/api/v1/clock/currentTime/byInput?hour=12&minutes=34
 	@GetMapping("/currentTime/byInput")
-	private String getTimetOnTextFormateByInputValue(@RequestParam Integer hour, @RequestParam Integer minutes ) throws InvalidTimeInputException  {
-		logger.info("ClockController :: called getTimetOnTextFormateByInputValue hour: "+ hour +" minutes : "+ minutes);
+	private String getTimetOnTextFormateByInputValue(@RequestParam Integer hour, @RequestParam Integer minutes)
+			throws InvalidTimeInputException {
+		logger.info(
+				"ClockController :: called getTimetOnTextFormateByInputValue hour: " + hour + " minutes : " + minutes);
 		return service.getCurrentTimeOnTextFormate(hour, minutes);
-				
+	}
+
+	// Rest api
+	// http://localhost:8083/api/v1/clock/currentTime/byTimeformateInput?time=01:05
+	@GetMapping("/currentTime/byTimeformateInput")
+	private String getTimetOnTextFormateByTimeformateInput(@RequestParam String time) throws InvalidTimeInputException {
+		if (time == null) {
+			throw new InvalidTimeInputException("Please check the time input");
+		}
+		String[] hm = time.split(":");
+		if (hm.length > 2) {
+			throw new InvalidTimeInputException("Please check the time input formate");
+		}
+
+		int hour = Integer.parseInt(hm[0]);
+		int minutes = Integer.parseInt(hm[1]);
+		logger.info("ClockController :: called getTimetOnTextFormateByTimeformateInput hour: " + hour + " minutes : "
+				+ minutes);
+		return service.getCurrentTimeOnTextFormate(hour, minutes);
+
 	}
 }
